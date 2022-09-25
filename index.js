99d15e4a2c8ac20e6c318e9d656bceb132cb9f88
Sharvil <karwasharvil@gmail.com>
2022-09-25 12:30:11 +0530

    first commit

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const postRoutes = require("./postRoutes");
-const productRoutes = require("./productRoutes");
+const ticketRoutes = require("./ticketRoutes");
 const app = express();
 const cors = require("cors");
 app.use(cors());
 
 mongoose
   .connect(
-    `mongodb+srv://12345:${process.env.PASSWORD}@cluster0.uvjuu4s.mongodb.net/?retryWrites=true&w=majority`,
+    `mongodb+srv://12345:${process.env.PASSWORD}@cluster0.mog0tlz.mongodb.net/?retryWrites=true&w=majority`,
     { useNewUrlParser: true }
   )
   .then(() => {
     const app = express();
     app.use(express.json());
-    app.use("/api", cors(), postRoutes);
-    app.use("/api", cors(), productRoutes);
+    app.use("/api", cors(), ticketRoutes);
     app.listen(process.env.PORT || 5000, () => {
       console.log("Server has started!");
     });
--- models/Product.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const schema = mongoose.Schema({
-  icon: String,
-  title: String,
-  content: String,
-});
-module.exports = mongoose.model("Product", schema);
